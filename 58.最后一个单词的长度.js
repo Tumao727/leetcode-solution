b1d9854a589bf2c7d689d3475a1fca31172978cc
tumao <tumao@protonmail.com>
2020-05-05 15:22:38 +0800

    feat(answer): finish lengthOfLastWord function

---
+++ 58.最后一个单词的长度.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=javascript
+ *
+ * [58] 最后一个单词的长度
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = (s) => {
+  const list = s.split(' ').filter(a => a);
+  const lastWord = list.length > 0 ? list[list.length - 1] : '';
+  return lastWord.length;
+};
+
+const result = lengthOfLastWord('heo')
+console.log('result', result)
+// @lc code=end
+
